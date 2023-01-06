5cd02a24cabb50dab4b6cd911d634659c2f4a2ec
Viswanatha Reddy <injaviswanathreddy@gmail.com>
2023-01-06 11:20:05 +0530

    component - selector

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers', //if this is used, then use selector as a <app-servers></app-servers> as element
+  //selector: '[app-servers]',   // if  this, then use as a element attribute <h1 app-servers>
+ // selector: '.app-servers',  // if this , then use element as class attribute of element
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+
+}
